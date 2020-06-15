bc691775ff7a56006a8ed6673ff6793dba516925
Sergey Pashin <S.E.Pashin@yandex.ru>
2020-06-15 07:03:34 +0300

    Начальный код

---
+++ js/main.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var createCard = [];
+
+var createAllCards = function (i) {
+  for (var i = 0; i < 8; i++) {
+
+  }
+}
+console.log(createCard);
+console.log(createAllCards());
+// По всей видимости это объект который включает в себя ключ и значение. И в то же время его кавычки намекают что этот объект сам является ключем а его значение - значением???
+var authorOject = {avatar : "img/avatars/user0"+ i + ".png",}
+
+var offerObject = {
+      titleKey: "<h1>строка, заголовок предложения</h1>",
+      addressKey : "600, 350",
+      priceKey: 47,
+      typeKey: "строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo???",
+      roomsKey: 1,
+      guestsroomsKey: 4,
+      checkinKey: "12:00, 13:00 , 14:00",
+      checkoutKey: "12:00, 13:00, 14:00",
+      featuresKey: ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"],
+      descriptionKey: "строка с описанием",
+      "photosKey": ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"],
+  }
+
+var locationObject = {
+      xKey: 10,
+      // случайное число, координата y метки на карте от 130 до 630.
+      yKey: 130,
+  }
+
+
+

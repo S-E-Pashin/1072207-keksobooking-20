5880a896be993e6b2d38bfa5f848f5d49a6103d4
Sergey Pashin <S.E.Pashin@yandex.ru>
2020-06-16 05:46:38 +0300

    Внесены изменения, добавлены пояснения для первых функций.

--- js/main.js
+++ js/main.js
@@ -1,36 +1,81 @@
-"use strict";
+/* eslint-disable no-labels */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+'use strict';
+var createCardArray = [];
+var locationAxis = {
+  axisX: 600,
+  axisY: 350,
+};
+var PRICE_ARRAY = [0, 1000, 5000, 10000];
+var TYPE_ROOM_ARRAY = ['palace', 'flat', 'house', 'bungalo'];
+var NUMBER_ROOMS_ARRAY = [1, 2, 3, 100];
+var NUMBER_GUESTS_ARRAY = [1, 2, 3, 0];
+var CHECKIN_ARRAY = ['12:00', '13:00', '14:00'];
+var CHECKOUT_ARRAY = ['12:00', '13:00', '14:00'];
+var FEATURES_ARRAY = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var photoArray = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+                'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+                'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var X_MIN = 0;
+var X_MAX = 1200;
+var Y_MIN = 130;
+var Y_MAX = 630;
 
-var createCard = [];
+var b = 1;
+var a = 1 + b;
+console.log(a);
+console.log(locationAxis.axisY);
 
-var createAllCards = function (i) {
-  for (var i = 0; i < 8; i++) {
+// Функция которая принимает в себя значение(параметр)/приравнивается к массивы и с принятым/полученным массивом выполняет математичесскую операцию округления аргумента(Результата умножения содержимого) до ближайшего меньшего целого. Содержимое составляет математичесская функция которая формирует случайное (псевдо) число от 0 до 1 и данное число будет умножено на округленное значение длинны выше принятого массива. Результатом данной функции будет число которое будет соответствовать одному из порядковых номеров указанного нами массива и может использоваться для условнорандомного формирования случайного элемента из данного массива.
+var getRandomArrayIndex = function (array) {
+  // var arrayLength = array.length;
+  return Math.floor(Math.random() * Math.floor(array.length));
+};
 
-  }
+// Функция которая принимает в себя 2а значения(параметра) 1й это минимальное число из необходимого диапазона а второе является максимальным числом. Функция округлит в большую или меньшую сторону (если >0,5  тогда в большую и наоборот) значение которое будет получено из содержимого. Содержимое: Функция предоставления рандомного числа умножена на разность между максимальным и минимальным числом и к полученному значению прибавляется значение минимального числа(для того чтобы не получить отрицательного числа). Результатом данной функции будет получение рандомного целого числа из указанного диапазона.
+
+function getRandomInteger(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
 }
-console.log(createCard);
-console.log(createAllCards());
-// По всей видимости это объект который включает в себя ключ и значение. И в то же время его кавычки намекают что этот объект сам является ключем а его значение - значением???
-var authorOject = {avatar : "img/avatars/user0"+ i + ".png",}
 
-var offerObject = {
-      titleKey: "<h1>строка, заголовок предложения</h1>",
-      addressKey : "600, 350",
-      priceKey: 47,
-      typeKey: "строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo???",
-      roomsKey: 1,
-      guestsroomsKey: 4,
-      checkinKey: "12:00, 13:00 , 14:00",
-      checkoutKey: "12:00, 13:00, 14:00",
-      featuresKey: ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"],
-      descriptionKey: "строка с описанием",
-      "photosKey": ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"],
+
+// Функция которая запускает цикл создания указанного (из диапазона) количества элементов. Элементы с каждым циклом формируются и их общее значение приравнивается(Является составляющим/ими элементами/объектами) переменной createCardObject(Данная переменная является объектом который с каждой итерацией создает уникальный объект/впитывает в себя значения) следующий шаг это запустить функцию и записать данный объект в массив createCardArray
+var createAllCards = function () {
+  for (var i = 0; i < 8; i++) {
+    // var xCopy = ;
+    var createCardObject = {
+      authorObjects : {
+        avatarKey: 'img/avatars/user0' + (i + 1) + '.png'
+      },
+      location: {
+        xObjectKey : getRandomInteger(X_MIN, X_MAX),
+        yObjectKey : getRandomInteger(Y_MIN, Y_MAX)
+      },
+      offerObject: {
+        titleObjectKey : 'Заголовок предложения',
+        // На данный момент решения о передаче данных из location не нашел.
+        addressObjectKey : "600, 350",
+        // Значение ключа берется из массива, номер элемента массива формируется посредством применения функции getRandomArrayIndex которая в свою очередь формирует рандомное число основываясь на полученных данны о данной функции а именнона ее длинне.
+        priceObjectKey : PRICE_ARRAY[getRandomArrayIndex(PRICE_ARRAY)],
+        typeObjectKey : TYPE_ROOM_ARRAY[getRandomArrayIndex(TYPE_ROOM_ARRAY)],
+        roomsObjectKey : NUMBER_ROOMS_ARRAY[getRandomArrayIndex(NUMBER_ROOMS_ARRAY)],
+        guestsObjectKey : NUMBER_GUESTS_ARRAY[getRandomArrayIndex(NUMBER_GUESTS_ARRAY)],
+        checkinObjectKey : CHECKIN_ARRAY[getRandomArrayIndex(CHECKIN_ARRAY)],
+        checkoutObjectKey : CHECKOUT_ARRAY[getRandomArrayIndex(CHECKOUT_ARRAY)],
+        //Ключу равно значение копии исходного массива FEATURESARRAY от которого отрезана часть его элементов. Это выполнено посредством метода slice которому задано минимальное значение 0 а максимальное(то до которого необходимо совершить вырезание необходимой части цикла) представлено в виде выполняемой функции предоставления рандомного числа элемента массива который основывается на полученной информации о данном массиве.
+        featuresObjectKey : FEATURES_ARRAY.slice(0, getRandomArrayIndex(FEATURES_ARRAY)),
+        descriptionObjectKey : 'строка с описанием',
+        photosObjectKey : photoArray.slice(0, getRandomArrayIndex(photoArray)),
+      }
+    }
+    createCardArray.push(createCardObject);
+    // var xCopy = createCardObject.location.xObjectKey;
+
   }
-
-var locationObject = {
-      xKey: 10,
-      // случайное число, координата y метки на карте от 130 до 630.
-      yKey: 130,
-  }
-
-
-
+  return createCardArray;
+}
+// Запускаем выполнение функции по формированию массива с объектами.
+createAllCards();
+// Выводим значение массива в консоль.
+console.log(createCardArray);
